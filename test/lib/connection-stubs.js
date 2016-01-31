4a63773f6a7db302c14ea93f459ce6f942d07cf9
Mert Dogar <mertdogar@gmail.com>
2016-01-31 20:22:55 +0200

    test(*): Add connection stubs

---
+++ test/lib/connection-stubs.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const sinon = require('sinon');
+const ChannelStub = require('./channel-stubs');
+
+
+exports.generate = function(opt_message) {
+    return {
+        createChannel: sinon.spy(() => Promise.resolve(ChannelStub.generate()))
+    };
+}
+
