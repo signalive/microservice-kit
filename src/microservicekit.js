ac10ccfc7555d5b1ceb718e97efd40363b663eb4
Deniz Gurkaynak <dgurkaynak@gmail.com>
2016-02-19 00:03:15 +0200

    refactor(microservicekit): Use random name in ids

--- src/microservicekit.js
+++ src/microservicekit.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const fs = require('fs');
 const uuid = require('node-uuid');
 const debug = require('debug')('microservicekit:microservicekit');
+const Chance = require('chance');
 
 const AmqpKit = require('./amqpkit');
 const ShutdownKit = require('./shutdownkit');
@@ -12,7 +13,7 @@ const ShutdownKit = require('./shutdownkit');
 class MicroserviceKit {
     constructor(opt_options) {
         this.options_ = _.assign({}, this.defaults, opt_options || {});
-        this.id = uuid.v4().split('-')[0];
+        this.id = new Chance().first().toLowerCase() + '-' + uuid.v4().split('-')[0];
         this.amqpKit = null;
         this.shutdownKit = ShutdownKit;
 
